7a8a530ee0a9f1632dec38e08f4f8c3bfa9dd00f
fredodiable <fredodiable@gmail.com>
2023-07-24 01:58:59 +0200

    Adding Counter-Strike: Global Offensive (CSGO)
    Adding Counter-Strike: Global Offensive (CSGO) widget page

--- src/config.ts
+++ src/config.ts
@@ -145,6 +145,7 @@ export const SIDEBAR: Sidebar = {
             ]},
             { text: 'Other', links: [
                 { text: 'Changedetection.io', link: 'en/services/changedetectionio'},
+                { text: 'Counter-Strike: Global Offensive', link: 'en/services/csgo'},
                 { text: 'EVCC', link: 'en/services/evcc'},                
                 { text: 'Fileflows', link: 'en/services/fileflows' },
                 { text: 'FreshRSS', link: 'en/services/freshrss' },
